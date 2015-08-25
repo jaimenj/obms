05425f99d854e64d2b586c128734475854dd2e40
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2015-08-25 18:11:18 +0200

    Security, API check.

--- src/ApiBundle/Controller/DefaultController.php
+++ src/ApiBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="api_home")
      * @Template()
      */
     public function indexAction()
