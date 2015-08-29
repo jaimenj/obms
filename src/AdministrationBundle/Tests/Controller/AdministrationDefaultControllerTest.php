0aca53ba85e0a7cc7b1e42d4b7668c31ca165552
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2015-08-29 16:31:47 +0200

    PHPUnit tests updated.

--- src/AdministrationBundle/Tests/Controller/AdministrationDefaultControllerTest.php
+++ src/AdministrationBundle/Tests/Controller/AdministrationDefaultControllerTest.php
@@ -4,11 +4,13 @@ namespace AdministrationBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class AdministrationControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
+        $client = static::createClient(array(), array(
+            'HTTPS' => true,
+        ));
 
         $crawler = $client->request('GET', '/administration/');
         $this->assertEquals(302, $client->getResponse()->getStatusCode());
--- src/ApiBundle/Tests/Controller/ApiControllerTest.php
+++ src/ApiBundle/Tests/Controller/ApiControllerTest.php
@@ -4,11 +4,15 @@ namespace ApiBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class ApiControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
+        $client = static::createClient(array(), array(
+            'HTTPS' => true,
+            'PHP_AUTH_USER' => 'user1',
+            'PHP_AUTH_PW'   => 'thepass',
+        ));
 
         $crawler = $client->request('GET', '/api/');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
