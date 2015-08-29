f036969460dcff5923804d853168c780ac97786e
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2015-08-29 12:48:11 +0200

    Initial admin.

---
+++ src/AdministrationBundle/DataFixtures/ORM/AdministratorFix.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AdministrationBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AdministrationBundle\Entity\Administrator;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class AdministratorFix extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    /**
+     *
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $factory = $this->container->get('security.encoder_factory');
+
+        $administrator = new Administrator();
+        $administrator->setUsername('admin');
+        $administrator->setEmail('admin@thedomainobms.com');
+        $encoder = $factory->getEncoder($administrator);
+        $password = $encoder->encodePassword('thepass', $administrator->getSalt());
+        $administrator->setPassword($password);
+
+        $manager->persist($administrator);
+        $manager->flush();
+    }
+    public function getOrder()
+    {
+        return 1;
+    }
+}
