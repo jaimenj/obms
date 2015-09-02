0a3acecebee3c0f351336efc16cb30c3d4500b79
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2015-09-02 20:56:48 +0200

    User and administrator CRUD for administration panel.

---
+++ src/AdministrationBundle/Form/AdministratorType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AdministrationBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class AdministratorType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('email')
+            ->add('password')
+            ->add('salt')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AdministrationBundle\Entity\Administrator'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'administrationbundle_administrator';
+    }
+}
