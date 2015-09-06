f0268a3c1527e8e00b0e21a33f6c64163059a33a
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2015-09-06 11:36:14 +0200

    Updating automatic tests.

--- src/AppBundle/Tests/Controller/AppControllerTest.php
+++ src/AppBundle/Tests/Controller/AppControllerTest.php
@@ -6,6 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class AppControllerTest extends WebTestCase
 {
+    public function setUp()
+    {
+        exec('php app/console doctrine:fixtures:load --no-interaction --env=test');
+        exec('php app/console sample:data --env=test');
+    }
+    
     public function testIndex()
     {
         $client = static::createClient(array(), array(
