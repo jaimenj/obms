5c984b6b70a759c0fe311d639e498b51c4077dbf
Jaime Niñoles-Manzanera Jimeno <info@jnjsite.com>
2015-09-20 23:59:13 +0200

    Referencing to the LICENSE in PHP files.

--- src/AdministrationBundle/DependencyInjection/Configuration.php
+++ src/AdministrationBundle/DependencyInjection/Configuration.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of The OBMS project: https://github.com/obms/obms
+ *
+ * Copyright (c) Jaime Niñoles-Manzanera Jimeno.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace AdministrationBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
--- src/AdministrationBundle/Tests/LoadFixturesTest.php
+++ src/AdministrationBundle/Tests/LoadFixturesTest.php
@@ -1,4 +1,13 @@
 <?php
+
+/**
+ * This file is part of The OBMS project: https://github.com/obms/obms
+ *
+ * Copyright (c) Jaime Niñoles-Manzanera Jimeno.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace BackBundle\Tests;
 
 use Liip\FunctionalTestBundle\Test\WebTestCase;
